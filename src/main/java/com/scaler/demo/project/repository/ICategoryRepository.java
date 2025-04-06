7839380bf38043d914b667eaaa66a98dfb2cc33b
pavankalyan <siramkalyan@gmail.com>
2025-04-06 17:33:47 +0530

    Format changes
    Format changes

--- src/main/java/com/scaler/demo/project/repository/ICategoryRepository.java
+++ src/main/java/com/scaler/demo/project/repository/ICategoryRepository.java
@@ -4,10 +4,12 @@ import com.scaler.demo.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category,Long> {
     Optional<Category> findByName(String name);
     Optional<Category> findById(Long id);
+
 }
